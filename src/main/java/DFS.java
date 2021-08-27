a8ad99a27ba7c35d66140369893249e5b7027305
tayefehalireza <tayefehalireza@gmail.com>
2021-08-27 17:23:02 +0430

    create AStarPathfinding class

--- src/main/java/DFS.java
+++ src/main/java/DFS.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * @author Alireza Tayefeh
@@ -8,7 +6,7 @@ import java.util.Queue;
 
 public class DFS {
     public static void main(String[] args) {
-        Graph graph = new Graph(10); // 10 nodes in this graph
+        Graph4 graph = new Graph4(10); // 10 nodes in this graph
         for (Node n : graph.nodes) {
             System.out.printf("Node index %d has value %d\n", n.index, n.value);
         }
@@ -48,12 +46,12 @@ class Node {
     }
 }
 
-class Graph {
+class Graph4 {
 
     int totalNode;
     Node[] nodes;
 
-    public Graph(int totalNode) {
+    public Graph4(int totalNode) {
         this.totalNode = totalNode;
         nodes = new Node[totalNode];
         for (int i = 0; i < totalNode; i++) {
